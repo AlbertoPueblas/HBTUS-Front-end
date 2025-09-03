aaa0500d5c6a1c67b7863b8664c5e679fbb216e1
Alberto Pueblas <154467649+AlbertoPueblas@users.noreply.github.com>
2025-09-03 12:23:11 +0200

    cambios url

--- hbtus/src/services/apiCalls.js
+++ hbtus/src/services/apiCalls.js
@@ -3,7 +3,7 @@ import axios from "axios"
 //---------------------------------------------
 
 // const API_URL = "http://localhost:3000/api/";
-const API_URL = "https://hbtu-bbdd-production.up.railway.app/api/";
+const API_URL = "https://hbtu-bbdd-production.up.railway.app/";
 
 export const activeProfile = async (active, token) => {
   const config = {
